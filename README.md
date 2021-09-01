9cd6e5c0fdc46d5619e168b3f56cbaafc580e6c1
Muskaan <75640403+xmuskaan@users.noreply.github.com>
2021-09-01 19:47:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HFY-Server
+# Merng
+ 
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://test:bWlbm4JobwQOF6bI@cluster0.rtgvn.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
